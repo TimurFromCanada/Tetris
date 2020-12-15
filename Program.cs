32a95d17d4db941de22c5ca15c5aa4d1502f0b3f
TimurFromCanada <timurfromcanada@gmail.com>
2020-12-15 13:01:05 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            Application.EnableVisualStyles(); //включаем визуальные стили приложения
+            Application.Run(new Form1()); //запуск Form1
+        }
+    }
+}
